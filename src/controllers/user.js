19ca7c1ea677594f857c91c3d5adbaeea2e3a06f
Tom Snelling <tomsnelling8@gmail.com>
2021-07-23 13:38:28 +0100

    allow registrations based on env var

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,60 +1,67 @@
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
 import crypto from 'crypto'
+import dotenv from 'dotenv'
 import User from '../schema/user'
 
+dotenv.config()
+
 export const register = async (req, res) => {
-  if (req.body.username && req.body.email && req.body.password) {
-    const created = Date.now()
+  if (process.env.SQ_ALLOW_REGISTER) {
+    if (req.body.username && req.body.email && req.body.password) {
+      const created = Date.now()
 
-    try {
-      const user = await User.findOne({
-        $or: [{ email: req.body.email }, { username: req.body.username }],
-      })
-
-      if (!user) {
-        const hash = await bcrypt.hash(req.body.password, 10)
-
-        const newUser = new User({
-          username: req.body.username,
-          email: req.body.email,
-          password: hash,
-          torrents: {},
-          created,
+      try {
+        const user = await User.findOne({
+          $or: [{ email: req.body.email }, { username: req.body.username }],
         })
 
-        newUser.uid = crypto
-          .createHash('sha256')
-          .update(newUser._id.toString())
-          .digest('hex')
-          .slice(0, 10)
+        if (!user) {
+          const hash = await bcrypt.hash(req.body.password, 10)
 
-        newUser.token = jwt.sign(
-          {
-            id: newUser._id,
-            created: created,
-          },
-          process.env.SQ_JWT_SECRET
-        )
-
-        const createdUser = await newUser.save()
-
-        if (createdUser) {
-          res.send({
-            token: createdUser.token,
-            id: createdUser._id,
-            uid: createdUser.uid,
+          const newUser = new User({
+            username: req.body.username,
+            email: req.body.email,
+            password: hash,
+            torrents: {},
+            created,
           })
+
+          newUser.uid = crypto
+            .createHash('sha256')
+            .update(newUser._id.toString())
+            .digest('hex')
+            .slice(0, 10)
+
+          newUser.token = jwt.sign(
+            {
+              id: newUser._id,
+              created: created,
+            },
+            process.env.SQ_JWT_SECRET
+          )
+
+          const createdUser = await newUser.save()
+
+          if (createdUser) {
+            res.send({
+              token: createdUser.token,
+              id: createdUser._id,
+              uid: createdUser.uid,
+            })
+          }
+        } else {
+          res.sendStatus(409)
         }
-      } else {
-        res.sendStatus(409)
+      } catch (err) {
+        console.error(err)
+        res.status(500).send(err.message)
       }
-    } catch (err) {
-      console.error(err)
-      res.status(500).send(err.message)
+    } else {
+      res.sendStatus(400)
     }
   } else {
-    res.sendStatus(400)
+    res.status(403).send('Registration closed')
   }
 }
 
