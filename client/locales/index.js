b66f2fb8d1c38ea1a36e7bdf92b73f0eb3196957
Mercurio <47455213+NotLugozzi@users.noreply.github.com>
2024-01-08 00:16:41 +0100

    Add italian to index.js

--- client/locales/index.js
+++ client/locales/index.js
@@ -1,12 +1,16 @@
 import en from "./en.json";
+import es from "./es.json";
 import ru from "./ru.json";
 import de from "./de.json";
 import zh from "./zh.json";
 import eo from "./eo.json";
 import fr from "./fr.json";
+import it from "./it.json";
 
 export default {
   en,
+  es,
+  it,
   ru,
   de,
   zh,
